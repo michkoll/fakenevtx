38217096eadf19fa09c2ea6171570df65385e32d
Michael Koll <michaelkoll@gmx.de>
2020-12-31 07:57:26 +0100

    usecase 001 finished, added doc

--- usecases/001_replace_username.py
+++ usecases/001_replace_username.py
@@ -2,7 +2,7 @@ from Workflow.ModifyStep import ModifyElementValueStep, ModifyEventdataStep
 from Workflow.Workflow import *
 
 
-def main(src, dest, old_username, new_username):
+def main(src, dest, old_username, new_username, new_sid):
     # initialize Workflow
     workflow = Workflow()
 
@@ -10,7 +10,9 @@ def main(src, dest, old_username, new_username):
     filter_subj = WorkflowStepFilter()
     filter_subj.add_eventdata_filter("SubjectUserName", old_username)
     # create and add step to workflow
+    step_subj_sid = ModifyEventdataStep(filter_subj, new_value=new_sid, eventdata_name="SubjectUserSid")
     step_subj = ModifyEventdataStep(filter_subj, new_value=new_username, eventdata_name="SubjectUserName")
+    workflow.add_step(step_subj_sid)
     workflow.add_step(step_subj)
 
     # create target username filter
@@ -18,7 +20,10 @@ def main(src, dest, old_username, new_username):
     filter_target.add_eventdata_filter("TargetUserName", old_username)
     # create and add step to workflow
     step_target = ModifyEventdataStep(filter_target, new_value=new_username, eventdata_name="TargetUserName")
+    step_target_sid = ModifyEventdataStep(filter_target, new_value=new_sid, eventdata_name="TargetUserSid")
+    workflow.add_step(step_target_sid)
     workflow.add_step(step_target)
+
 
     # start workflow
     workflow.run(src, dest)
@@ -29,7 +34,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Replace a given username with new value.")
     parser.add_argument("src", type=str, help="Path to the source Windows EVTX event log file")
     parser.add_argument("dest", type=str, help="Path to the source Windows EVTX event log file")
-    parser.add_argument("old_username", type=str, help="Path to the source Windows EVTX event log file")
-    parser.add_argument("new_username", type=str, help="Path to the source Windows EVTX event log file")
+    parser.add_argument("old_username", type=str, help="Old username")
+    parser.add_argument("new_username", type=str, help="New username")
+    parser.add_argument("new_sid", type=str, help="SID of new user")
     args = parser.parse_args()
-    main(args.src, args.dest, args.old_username, args.new_username)
+    main(args.src, args.dest, args.old_username, args.new_username, args.new_sid)
--- usecases/README.md
+++ usecases/README.md
@@ -1,9 +1,29 @@
 # Use Cases
 
+Please make sure, that all installation requirements (e.g. installed packages in virtual environment) are fulfilled
+
 ## 001 Replace username
 
 See [001_replace_username.py](001_replace_username.py)
 
 ```shell
+$ python3 -m usecases.001_replace_username -h
+usage: 001_replace_username.py [-h] src dest old_username new_username new_sid
 
+Replace a given username with new value.
+
+positional arguments:
+  src           Path to the source Windows EVTX event log file
+  dest          Path to the source Windows EVTX event log file
+  old_username  Old username
+  new_username  New username
+  new_sid       SID of new user
+
+optional arguments:
+  -h, --help    show this help message and exit
 ```
+
+| Original      | Result  |
+| ------------- |:-------------:| 
+| ![Use Case 001 - Original](../doc/images/001_original.png "Use Case 001 - Original")     | ![Use Case 001 - Result](../doc/images/001_result.png "Use Case 001 - Result") | 
+
